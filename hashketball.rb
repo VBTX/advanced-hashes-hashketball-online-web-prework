8045dd83ca4a4f1e7a60a017c9d31c9fe0d970c3
Victoria B <viktorija.bourassa@gmail.com>
2018-12-22 20:49:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -232,9 +232,8 @@ end
 
 def big_shoe_rebounds
     game_hash[:home][:players].each do |key, value|
-      obj = value
-      obj.each do |key1, val|
-        return val
+      value.each  do |k, v|
+        k
       end
         end
   end
